fec293157630263ff6ffd74a4943d88f63c28d3e
Benjamin Logue and Kevin VanEvery <kevin@>
2016-06-09 08:53:59 -0700

    add support for evaluating single digit integers into roman numeral

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,44 @@
 /*This file is for your custom js.  All yours*/
+var romanNumeral = function(integer){
+  if (integer >= 4000) {
+    return false;
+  }
 
+  var string = integer.toString();
+  var array = string.split();
+  var reverseArray = array.reverse();
+  debugger;
+  if (reverseArray[0] === "0") {
+    reverseArray[0] = "";
+  } else if (reverseArray[0] === "1") {
+    reverseArray[0] = "I";
+  } else if (reverseArray[0] === "2") {
+    reverseArray[0] = "II";
+  } else if (reverseArray[0] === "3") {
+    reverseArray[0] = "III";
+  } else if (reverseArray[0] === "4") {
+    reverseArray[0] = "IV";
+  } else if (reverseArray[0] === "5") {
+    reverseArray[0] = "V";
+  } else if (reverseArray[0] === "6") {
+    reverseArray[0] = "VI";
+  } else if (reverseArray[0] === "7") {
+    reverseArray[0] = "VII";
+  } else if (reverseArray[0] === "8") {
+    reverseArray[0] = "VIII";
+  } else if (reverseArray[0] === "9") {
+    reverseArray[0] = "IX";
+  }
+
+  var output = reverseArray.join("");
+  return output;
+}
 
 $(document).ready(function(){
 
   $("#user-number").submit(function(event){
     event.preventDefault();
-    var userNumber = $("#arabic-numeral").val();
+    var userNumber = parseInt($("#arabic-numeral").val());
 
     var output = romanNumeral(userNumber);
 
