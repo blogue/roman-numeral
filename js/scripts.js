ba8d53d173567c3b59b09562c5dc9809d156b7f5
Benjamin Logue and Kevin VanEvery <kevin@>
2016-06-09 08:33:26 -0700

    add front-end logic to javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 /*This file is for your custom js.  All yours*/
+
+
+$(document).ready(function(){
+
+  $("#user-number").submit(function(event){
+    event.preventDefault();
+    var userNumber = $("#arabic-numeral").val();
+
+    var output = romanNumeral(userNumber);
+
+    $('#output').text(output);
+  });
+});
